264163c7fa67e16306622541334488f7f4c58abf
iamrvp <148988907+iamrvp@users.noreply.github.com>
2024-03-15 09:41:59 +0000

    detatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict" //treat all JS as newer version
+
+//alert(3+3)// we are using nodejs, not browser
+
+console.log(3+3)
+// code readablity should be high
+console.log("Rushsikesh")
+
+let name = "rushikesh"
+let age ="26"
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bihint 
+// string => ""
+// boolean => ture/false
+// null => stand alone value
+// undefine => 
+// symbol = unique
+
+
+// object
+
+
+console.log(typeof = "Rushikesh")
