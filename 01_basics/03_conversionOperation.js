3e7d7742b9310dfb12d0dcb2a76c5a0169e6f14d
iamrvp <148988907+iamrvp@users.noreply.github.com>
2024-03-16 07:34:53 +0000

    conversion of deta Type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "Rushikesh"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumberm = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// "33"=> 33
+//"33abc"=> NaN
+// ture => 1 false=> 0
+
+//let isLoggedIn = 1
+
+//let booleanIsLoggedIn = Boolean(is LoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => ture 0=> false
+//""=> false
+//"Rushikehs" => Ture
+
+let stringNumber + stringNumber(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
